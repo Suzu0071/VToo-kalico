62a9a1885867c1beb9b306c716d886d20c66d4dc
Suzu0071 <pakost20@gmail.com>
2025-09-30 16:54:39 -0600

    fixed typo

--- klippy/kinematics/corexy.py
+++ klippy/kinematics/corexy.py
@@ -20,7 +20,7 @@ class CoreXYKinematics:
         self.rails[0].setup_itersolve("corexy_stepper_alloc", b"+")
         self.rails[1].setup_itersolve("corexy_stepper_alloc", b"-")
         self.rails[2].setup_itersolve("cartesian_stepper_alloc", b"z")
-        self.rails[3].setup_itersolve("cartesian_stepper_accoc", b"z")
+        self.rails[3].setup_itersolve("cartesian_stepper_accoc", b"a")
         for s in self.get_steppers():
             s.set_trapq(toolhead.get_trapq())
             toolhead.register_step_generator(s.generate_steps)
